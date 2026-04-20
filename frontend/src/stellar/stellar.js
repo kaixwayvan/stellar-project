c118f58f8b1a9ccd12561bf3bcfe58699b9f41fc
Khurt Daniel Sore <142791082+kaixwayvan@users.noreply.github.com>
2026-04-20 12:49:47 +0800

    move baonlock to src and fix imports

--- frontend/src/stellar/stellar.js
+++ frontend/src/stellar/stellar.js
@@ -1,4 +1,4 @@
-import * as BaonLock from "./baonlock-logic.js";
+import * as BaonLock from "../baonlock/dist/index.js";
 import { isConnected, getAddress, setAllowed, signTransaction } from "@stellar/freighter-api";
 
 export const client = new BaonLock.Client({
